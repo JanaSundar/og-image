7c61d72c1a1000aadcce3de1c3df605ff862aaef
Janarthanan <janasundar1997@gmail.com>
2021-10-13 11:48:58 +0530

    Chrome undefined bug fixed

--- utils/getPage.js
+++ utils/getPage.js
@@ -4,41 +4,12 @@ let page;
 
 const isDev = process.env.NODE_ENV === 'development';
 
-const exePath =
-  process.platform === 'win32'
-    ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-    : process.platform === 'linux'
-    ? '/usr/bin/google-chrome'
-    : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-
 export const getPage = async () => {
   if (page) {
     return page;
   }
 
-  const getOptions = async () => {
-    let options;
-    if (isDev) {
-      options = {
-        args: [],
-        executablePath: exePath,
-        headless: true,
-      };
-    } else {
-      options = {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-      };
-    }
-
-    return options;
-  };
-
-  const options = await getOptions();
-  page = await Playwright.launchChromium({
-    ...options,
-  });
+  page = await Playwright.launchChromium();
 
   return page;
 };
