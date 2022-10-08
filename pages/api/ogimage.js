4090545c5406bb52acc9b0af1247e3797ad9d506
Janarthanan <janasundar1997@gmail.com>
2022-10-08 10:54:11 +0530

    New Api added to generate an image

---
+++ pages/api/ogimage.js
@@ -0,0 +1,62 @@
+import chromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
+
+export default async function handler(req, res) {
+  try {
+    const { title, color, background } = req.query;
+
+    const isDev = process.env.NODE_ENV === 'development';
+    const exePath =
+      process.platform === 'win32'
+        ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+        : process.platform === 'linux'
+        ? '/usr/bin/google-chrome'
+        : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+    const baseUrl = isDev ? 'http://localhost:3001' : 'http://og-image.janasundar.dev';
+
+    const getOptions = async () => {
+      let options;
+      if (isDev) {
+        options = {
+          args: [],
+          executablePath: exePath,
+          headless: true,
+        };
+      } else {
+        options = {
+          args: chromium.args,
+          executablePath: await chromium.executablePath,
+          headless: chromium.headless,
+        };
+      }
+
+      return options;
+    };
+
+    const qs = new URLSearchParams({
+      title,
+      color,
+      background,
+    });
+    const url = `${baseUrl}?${qs.toString()}`;
+    const options = await getOptions();
+
+    const browser = await puppeteer.launch({
+      ...options,
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 630 });
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    const buffer = await page.screenshot({ type: 'png' });
+    await browser.close();
+
+    res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
+    res.setHeader('Content-Type', 'image/png');
+    res.end(buffer);
+  } catch (err) {
+    console.error(err);
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
+  }
+}
